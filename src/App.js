fbcde9a5efd206d2892781502617cee0ff385316
mohamedosama2 <latokalatohamato@gmail.com>
2021-06-08 07:29:15 +0200

    react form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from './Components/Validation/Form'
 
 function App() {
+  const inputs = [
+    { name: "username", type: "text", placeholder: "Full Name" },
+    { name: "phone", type: "phone", placeholder: "Phone Number" },
+    { name: "password", type: "password", placeholder: "Password" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form
+      submitHandler={(data) => console.log(data)}
+      inputs={inputs}
+      datePicker={true}
+    />
   );
 }
 
---
+++ src/Components/Validation/Form.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+import "./Form.css";
+const phoneRegExp = /^01[0125][0-9]{8}$/;
+
+function AddTrainer(props) {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const shape = {};
+
+  props.inputs.forEach((i) => {
+    let name = i["name"];
+    let values = [];
+
+    if (i["type"]) values.push(i["type"]);
+    else values.push("text");
+
+    if (i["notRequired"]) values.push("notRequired");
+    else values.push("required");
+
+    if (
+      values[0] === "string" ||
+      values[0] === "text" ||
+      values[0] === "password"
+    ) {
+      if (values[1] === "notRequired") {
+        shape[[name]] = yup.string().notRequired();
+      } else {
+        shape[[name]] = yup.string().required();
+      }
+    } else if (values[0] === "number") {
+      shape[[name]] = yup.string().notRequired();
+      if (values[1] === "notRequired") {
+      } else {
+        shape[[name]] = yup.string().required();
+      }
+    } else if (values[0] === "phone") {
+      if (values[1] === "notRequired") {
+        shape[[name]] = yup
+          .string()
+          .matches(phoneRegExp, "Phone number is not valid")
+          .notRequired();
+      } else {
+        shape[[name]] = yup
+          .string()
+          .matches(phoneRegExp, "Phone number is not valid")
+          .required();
+      }
+    } else if (values[0] === "email") {
+      if (values[1] === "notRequired") {
+        shape[[name]] = yup.string().notRequired().email();
+      } else {
+        shape[[name]] = yup.string().required().email();
+      }
+    }
+  });
+
+  const schema = yup.object().shape(shape);
+  console.log(shape);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  return (
+    <form
+      onSubmit={handleSubmit(props.submitHandler)}
+      className={props.formStyle}
+    >
+      {props.inputs.map((input, key) => {
+        return (
+          <div key={key}>
+            <input
+              name={input.name}
+              placeholder={input.placeholder}
+              type={input.type}
+              className={props.inputStyle}
+              {...register(input.name)}
+              style={{
+                border: errors[input.name] ? "1px solid rgb(172, 50, 50)" : "",
+              }}
+            />
+            {errors[input.name]?.message ? (
+              <p className="message">{errors[input.name]?.message}</p>
+            ) : (
+              ""
+            )}
+          </div>
+        );
+      })}
+      {props.datePicker ? (
+        <div style={{ width: "50%" }}>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+
+      <button type="submit">Add Trainer</button>
+    </form>
+  );
+}
+
+export default AddTrainer;
